eec36e171316537e53a4d2a7ecf6d0351dfd1595
GeneralNonameOfBoogaBooga <petricenkovadim3@gmail.com>
2026-04-20 21:33:29 +0500

    Update BW.lua

--- BW.lua
+++ BW.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/392c3d8840dd45aeb0702aa9de793c008770e5c8433b2facf540a6e5e8258651/download"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/fa4c95b4955f5f7d67b82138c058281d3c97101f501a4535c8af8ebb00bbc70c/download"))()
